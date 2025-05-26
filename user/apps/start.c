c90c25d8c79f8b37f3fe9c576ed532a76be1d186
Héloïs <127110080+hlelo101@users.noreply.github.com>
2025-05-26 13:35:17 +0200

    Fix start.c on Bochs

--- user/apps/start.c
+++ user/apps/start.c
@@ -1,8 +1,9 @@
 #include <acky.h>
 
+char uinput[32] = {0};
+
 void main() {
     print("Enter the path of the program to run: ");
-    char uinput[32];
     getInput(uinput, sizeof(uinput));
 
     if(spawnProcess(uinput) == 1) print("An error occurred\n");
